8286d59a9f8cc70d6dfe5152a73e0abfedfcd200
Adil Adilkhanov <020527550543@stud.satbayev.university>
2022-05-16 00:11:57 +0600

    lesson_03

---
+++ src/lesson_03/ListInfoClass.java
@@ -0,0 +1,21 @@
+package lesson_03;
+
+import java.util.ArrayList;
+
+public class ListInfoClass {
+    public static void main(String[] args) {
+        // ArrayList
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("10");
+//        list.add("20");
+//        list.add("30");
+//        list.remove(1);
+//        list.set(1, "40");
+//        list.add("30");
+//        System.out.println(list);
+//        System.out.println(list.size());
+
+        // LinkedList
+
+    }
+}
