1547abf2cc4197f44ba1f0fb26feb54cfc4aa0f6
Adil Adilkhanov <020527550543@stud.satbayev.university>
2022-05-23 00:00:33 +0600

    lesson_07

---
+++ src/lesson_07/io/Challange.java
@@ -0,0 +1,11 @@
+package lesson_07.io;
+
+import java.io.File;
+
+public class Challange {
+    public static void main(String[] args) {
+        File file = new File("src/lesson_07/io/lorem.txt");
+
+
+    }
+}
